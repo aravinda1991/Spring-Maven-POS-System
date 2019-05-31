e805f6c66c24bdebb008abe79154a0f23c7e198a
Pramith <aravindapramith3@gmail.com>
2019-05-31 16:49:16 +0530

    create project

---
+++ src/main/java/lk/ijse/pos/main/AppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.pos.main;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan("lk.ijse.pos")
+@Configuration
+public class AppConfig {
+}
